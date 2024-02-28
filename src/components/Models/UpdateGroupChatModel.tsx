8eb8a30918b9c81fdde4c0efb44526801e8e5dba
Nikhil7282 <nikhilsabbz@gmail.com>
2024-02-28 16:47:16 +0530

    editGroup

--- src/components/Models/UpdateGroupChatModel.tsx
+++ src/components/Models/UpdateGroupChatModel.tsx
@@ -5,11 +5,20 @@ import Fade from "@mui/material/Fade";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import SettingsIcon from "@mui/icons-material/Settings";
-import { useAuthChat } from "../../hooks/contextHooks";
-import { useState } from "react";
+import { useAuthChat, useAuthUser } from "../../hooks/contextHooks";
+import { useEffect, useState } from "react";
 import UserBadge from "../UserBadge";
 import { User } from "../../context/AuthContext";
 import { FormControl, TextField } from "@mui/material";
+import toast from "react-hot-toast";
+import {
+  axiosAddToGroup,
+  axiosDeleteFromGroup,
+  axiosGroupName,
+  axiosSearchUsers,
+} from "../../axios/axiosClient";
+import UserListItem from "../UserListItem";
+import { getLocalStorage } from "../../hooks/storageHooks";
 
 const style = {
   position: "absolute" as "absolute",
@@ -27,17 +36,95 @@ const style = {
   justifyContent: "center",
 };
 
-const handleRemoveUser = (user: User) => {};
-const handleRename = () => {};
-const handleUserSearch = () => {};
 export default function UpdateGroupChatModel() {
   const chat = useAuthChat();
+  const auth = useAuthUser();
   const [open, setOpen] = useState(false);
   const [groupChatName, setGroupChatName] = useState("");
   const [search, setSearch] = useState("");
-  const [searchResult, setSearchResult] = useState([]);
+  const [searchResult, setSearchResult] = useState<User[]>([]);
   const [loading, setLoading] = useState(false);
   const [renameloading, setRenameLoading] = useState(false);
+
+  useEffect(() => {
+    const getUsers = setTimeout(async () => {
+      if (!search) return;
+      setLoading(true);
+      const res = await axiosSearchUsers(search);
+      setSearchResult(res);
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(getUsers);
+  }, [search]);
+
+  const handleRemoveUser = async (user: User) => {
+    if (!chat?.selectedChat?.users.find((u) => u._id === user._id)) {
+      toast("User Does Not Exists", {
+        icon: "⚠️",
+      });
+    }
+    try {
+      setLoading(true);
+      const res = await axiosDeleteFromGroup(
+        chat?.selectedChat?._id || "",
+        user._id || ""
+      );
+      setLoading(false);
+      toast.success(res.message);
+    } catch (error: any) {
+      setLoading(false);
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
+  };
+  const handleAddUser = async (user: User) => {
+    if (chat?.selectedChat?.users.find((u) => u._id === user._id)) {
+      toast.error("User already in group");
+      return;
+    }
+    try {
+      setLoading(true);
+      let res = await axiosAddToGroup(
+        chat?.selectedChat?._id || "",
+        user._id || ""
+      );
+      setLoading(false);
+      toast.success(res.message);
+    } catch (error: any) {
+      setLoading(false);
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
+  };
+  const handleGroupRename = async () => {
+    if (!groupChatName) {
+      toast("Enter Group Name", {
+        icon: "⚠️",
+      });
+      return;
+    }
+    try {
+      setRenameLoading(true);
+      const res = await axiosGroupName(
+        chat?.selectedChat?._id || "",
+        groupChatName
+      );
+      toast.success(res.message);
+      setRenameLoading(false);
+      if (chat?.selectedChat) {
+        chat?.setSelectedChat({
+          ...chat.selectedChat,
+          chatName: groupChatName,
+        });
+      }
+      setOpen(false);
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
+  };
+  // const handleUserSearch = () => {};
   return (
     <div>
       <Button onClick={() => setOpen(true)}>
@@ -74,6 +161,7 @@ export default function UpdateGroupChatModel() {
               {chat?.selectedChat?.users.map((user) => {
                 return (
                   <UserBadge
+                    key={user._id}
                     user={user}
                     handleFunction={() => handleRemoveUser(user)}
                   />
@@ -89,19 +177,35 @@ export default function UpdateGroupChatModel() {
               <Button
                 variant="contained"
                 sx={{ marginLeft: "3px", width: "30%" }}
-                onClick={handleRename}
+                onClick={handleGroupRename}
               >
                 Rename
               </Button>
             </Box>
-            <Box sx={{ margin: "15px" }}>
-              <TextField
-                label="Add User"
-                onChange={handleUserSearch}
-                size="small"
-              />
-            </Box>
-            <Box>List of Users</Box>
+            {chat?.selectedChat?.groupAdmin._id ===
+            getLocalStorage("userInfo").id ? (
+              <>
+                <Box sx={{ margin: "15px" }}>
+                  <TextField
+                    label="Add User"
+                    onChange={(e) => setSearch(e.target.value)}
+                    size="small"
+                  />
+                </Box>
+                <Box>
+                  {searchResult?.map((user) => (
+                    <UserListItem
+                      key={user._id}
+                      user={user}
+                      handleFunction={() => handleAddUser(user)}
+                    />
+                  ))}
+                </Box>
+              </>
+            ) : (
+              <></>
+            )}
+
             <Box>
               <Button>Leave</Button>
               <Button variant="contained" onClick={() => setOpen(false)}>
